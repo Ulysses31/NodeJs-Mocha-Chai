4a1002b53af64efd3bcacd3c26d63a426db2eba2
Iordanidis Christos <iordanidischr@gmail.com>
2021-03-23 14:20:13 +0200

    fix

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ var converter = require('./app/converter');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(function (req, res, next) {
-  // request
-  console.log(
-    JSON.stringify({
-      headers: req.headers,
-      remote: req._remoteAddress,
-      url: req.url,
-      method: req.method,
-      body: req.body
-    })
-  );
-  // response
-  res.on('finish', () => {
-    console.log(
-      JSON.stringify({
-        statusCode: res.statusCode,
-        statusMessage: res.statusMessage,
-        headers: res._header,
-        body: res.send
-      })
-    );
-  });
-  next();
-});
+// app.use(function (req, res, next) {
+//   // request
+//   console.log(
+//     JSON.stringify({
+//       headers: req.headers,
+//       remote: req._remoteAddress,
+//       url: req.url,
+//       method: req.method,
+//       body: req.body
+//     })
+//   );
+//   // response
+//   res.on('finish', () => {
+//     console.log(
+//       JSON.stringify({
+//         statusCode: res.statusCode,
+//         statusMessage: res.statusMessage,
+//         headers: res._header,
+//         body: res.send
+//       })
+//     );
+//   });
+//   next();
+// });
 
 app.get('/rgbToHex', function (req, res) {
   var red = parseInt(req.query.red, 10);
